e1ea03b7eb19965f5c49b5f53918b96bba96cb1e
fucek2 <fucek2@stud.uniza.sk>
2024-02-28 20:39:20 +0100

    init of new repo from template

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,16 +8,13 @@ import { useEffect } from 'react';
 import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
   initialRouteName: '(tabs)',
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -26,7 +23,6 @@ export default function RootLayout() {
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
