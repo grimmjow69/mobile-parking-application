a7d08c394e59d71aacba276d8ffcc4931f50491b
fucek2 <fucek2@stud.uniza.sk>
2024-03-21 13:51:06 +0100

    about page

--- app/about.tsx
+++ app/about.tsx
@@ -1,18 +1,75 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import i18n from '../assets/localization/i18n';
+import { Text, useTheme } from 'react-native-paper';
 
 export default function AboutScreen() {
+  const { colors } = useTheme();
   return (
     <View style={styles.container}>
-      <SafeAreaProvider style={styles.container}></SafeAreaProvider>
+      <SafeAreaProvider>
+        <ScrollView>
+          <Text
+            variant="headlineSmall"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.title')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content1')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content2')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content3')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content4')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content5')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.content6')}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={[{ color: colors.tertiary }, styles.text]}
+          >
+            {i18n.t('about.end')}
+          </Text>
+        </ScrollView>
+      </SafeAreaProvider>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16
+    paddingHorizontal: 12,
+    paddingVertical: 8
+  },
+  text: {
+    marginBottom: 12,
+    textAlign: 'center'
   }
 });
